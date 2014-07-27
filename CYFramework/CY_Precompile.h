bb0d3a578264c278002dbee4077f1fba5d393025
张海迪 <870347271@qq.com>
2014-07-27 23:14:07 +0800

    增加NSObject+CYMessage

---
+++ CYFramework/CY_Precompile.h
@@ -0,0 +1,24 @@
+//
+//  CY_Precompile.pch
+//  CYFramework
+//
+//  Created by 张海迪 on 14-7-27.
+//  Copyright (c) 2014年 haidi. All rights reserved.
+//
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 60000
+
+#define UILineBreakMode					NSLineBreakMode
+#define UILineBreakModeWordWrap			NSLineBreakByWordWrapping
+#define UILineBreakModeCharacterWrap	NSLineBreakByCharWrapping
+#define UILineBreakModeClip				NSLineBreakByClipping
+#define UILineBreakModeHeadTruncation	NSLineBreakByTruncatingHead
+#define UILineBreakModeTailTruncation	NSLineBreakByTruncatingTail
+#define UILineBreakModeMiddleTruncation	NSLineBreakByTruncatingMiddle
+
+#define UITextAlignmentLeft				NSTextAlignmentLeft
+#define UITextAlignmentCenter			NSTextAlignmentCenter
+#define UITextAlignmentRight			NSTextAlignmentRight
+#define	UITextAlignment					NSTextAlignment
+
+#endif	// #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 60000
---
+++ CYFramework/mvc/controller/extension/NSObject+CYMessage.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+CYMessage.m
+//  CYFramework
+//
+//  Created by 张海迪 on 14-7-27.
+//  Copyright (c) 2014年 haidi. All rights reserved.
+//
+
+#import "NSObject+CYMessage.h"
+
+@implementation NSObject (CYMessage)
+
+@end
