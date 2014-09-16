6d180d4f4daed806387b1307d0da44f8c1cdfbdf
张海迪 <870347271@qq.com>
2014-09-16 22:46:05 +0800

    增加宏

--- CYFramework/CY_Precompile.h
+++ CYFramework/CY_Precompile.h
@@ -22,3 +22,8 @@
 #define	UITextAlignment					NSTextAlignment
 
 #endif	// #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 60000
+
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+#define kScreenWidth      CGRectGetWidth([UIScreen mainScreen].applicationFrame)
+#define kScreenHeight     CGRectGetHeight([UIScreen mainScreen].applicationFrame)
